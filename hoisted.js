b4d020efb5b4111d3360aefa2e302504831bffe6
trebek1 <trebek08@gmail.com>
2015-07-06 11:30:09 -0700

    added a hoised example

---
+++ hoisted.js
@@ -0,0 +1,7 @@
+var text = 'outside'; 
+
+function logIt(){
+    console.log(text); 
+    var text = 'inside'
+}
+logIt(); 
--- trie2.js
+++ trie2.js
@@ -25,6 +25,73 @@ trie.prototype.add = function(word) {
     current.$ = 1;  //word end marker
 };
 
+trie.prototype.hasWord = function(word) {
+    this.validate(word);
+ 
+    var current = this.head;
+ 
+    for (var i = 0; i < word.length; i++) { 
+        if(!(word[i] in current)) {
+            return false;
+        }
+ 
+        current = current[word[i]]
+    };
+ 
+    return current.$ === 1; //word end marker
+};
+
+trie.prototype.remove = function(word) {
+    this.validate(word);
+ 
+    canDelete(word, -1, this.head);
+ 
+    function canDelete(word, index, node){ 
+        if(word === undefined ) throw "Bad Word";
+        if(index >= word.length) throw "Bad index to check for deletion.";
+        if(node === undefined ) throw "Bad Node at " + index + " for " + word;
+ 
+        if(index === word.length - 1) { 
+            //last letter
+            //always delete word marker (as we are deleting word)
+            return (delete node.$) && noKids(node); //if last letter of word, should be empty.
+        }
+        else {
+            //any other letter in word
+            //check child, and after child check, I am now empty
+            if(canDelete(word, index + 1, node[word[index + 1]]) )
+            {
+                //delete me 
+                return (delete node[word[index + 1]]) && noKids(node); 
+            }
+        }
+        return false;
+    };
+ 
+    function noKids(node) { 
+        return Object.keys(node).length === 0;
+    };
+};
+
+trie.prototype.sort = function() {
+    var word = "";
+    var sorted = [];
+ 
+    sortTrie(this.head, word, sorted);
+ 
+    function sortTrie(node, word, sorted) {
+        for(var letter in node) { 
+            if (letter == '$') { sorted.push(word); }
+            else { 
+                sortTrie(node[letter], word + letter, sorted); 
+            }
+        }
+    }
+ 
+    console.log(sorted);
+    return sorted;
+};
+
 var a = new trie(); 
 
 a.add('apple');
