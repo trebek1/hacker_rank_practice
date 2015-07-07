3bf2dfd803b5eef7c765637b2c94e3efe33843c1
trebek1 <trebek08@gmail.com>
2015-07-07 14:05:38 -0700

    sorts

---
+++ sorts.js
@@ -0,0 +1,197 @@
+// BubbleSort
+
+function bubbleSort(arr){
+   var len = arr.length;
+   for (var i = len-1; i>=0; i--){
+     for(var j = 1; j<=i; j++){
+       if(arr[j-1]>arr[j]){
+           var temp = arr[j-1];
+           arr[j-1] = arr[j];
+           arr[j] = temp;
+        }
+     }
+   }
+   return arr;
+}
+bubbleSort([7,5,2,4,3,9]); //[2, 3, 4, 5, 7, 9]
+bubbleSort([9,7,5,4,3,1]); //[1, 3, 4, 5, 7, 9]
+bubbleSort([1,2,3,4,5,6]); //[1, 2, 3, 4, 5, 6]
+
+//Selection Sort
+
+function selectionSort(arr){
+  var minIdx, temp, 
+      len = arr.length;
+  for(var i = 0; i < len; i++){
+    minIdx = i;
+    for(var  j = i+1; j<len; j++){
+       if(arr[j]<arr[minIdx]){
+          minIdx = j;
+       }
+    }
+    temp = arr[i];
+    arr[i] = arr[minIdx];
+    arr[minIdx] = temp;
+  }
+  return arr;
+}
+
+//Insertion Sort
+
+function insertionSort(arr){
+  var i, len = arr.length, el, j;
+
+  for(i = 1; i<len; i++){
+    el = arr[i];
+    j = i;
+
+    while(j>0 && arr[j-1]>toInsert){
+      arr[j] = arr[j-1];
+      j--;
+   }
+
+   arr[j] = el;
+  }
+
+  return arr;
+}
+
+//Merge Sort 
+
+function mergeSort(arr){
+   var len = arr.length;
+   if(len <2)
+      return arr;
+   var mid = Math.floor(len/2),
+       left = arr.slice(0,mid),
+       right =arr.slice(mid);
+   //send left and right to the mergeSort to broke it down into pieces
+   //then merge those
+   return merge(mergeSort(left),mergeSort(right));
+}
+
+//Merge 
+
+function merge(left, right){
+  var result = [],
+      lLen = left.length,
+      rLen = right.length,
+      l = 0,
+      r = 0;
+  while(l < lLen && r < rLen){
+     if(left[l] < right[r]){
+       result.push(left[l++]);
+     }
+     else{
+       result.push(right[r++]);
+    }
+  }  
+  //remaining part needs to be addred to the result
+  return result.concat(left.slice(l)).concat(right.slice(r));
+}
+        
+//QuickSort
+
+function quickSort(arr, left, right){
+   var len = arr.length, 
+   pivot,
+   partitionIndex;
+
+
+  if(left < right){
+    pivot = right;
+    partitionIndex = partition(arr, pivot, left, right);
+    
+   //sort left and right
+   quickSort(arr, left, partitionIndex - 1);
+   quickSort(arr, partitionIndex + 1, right);
+  }
+  return arr;
+}
+
+
+// Partition 
+
+function partition(arr, pivot, left, right){
+   var pivotValue = arr[pivot],
+       partitionIndex = left;
+
+   for(var i = left; i < right; i++){
+    if(arr[i] < pivotValue){
+      swap(arr, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(arr, right, partitionIndex);
+  return partitionIndex;
+}
+
+
+// Swap 
+
+function swap(arr, i, j){
+   var temp = arr[i];
+   arr[i] = arr[j];
+   arr[j] = temp;
+}
+
+
+quickSort([11,8,14,3,6,2,7],0,6); 
+//[2, 3, 6, 7, 8, 11, 14]
+quickSort([11,8,14,3,6,2,1, 7],0,7);
+//[1, 2, 3, 6, 7, 8, 11, 14]
+quickSort([16,11,9,7,6,5,3, 2],0,7);
+//[2, 3, 5, 6, 7, 9, 11, 16]
+        
+
+
+ // Heap Sort 
+
+ function heapSort(arr){
+  var len = arr.length,
+      end = len-1;
+
+  heapify(arr, len);
+  
+  while(end > 0){
+   swap(arr, end--, 0);
+   siftDown(arr, 0, end);
+  }
+  return arr;
+}
+        
+
+function heapify(arr, len){
+   // break the array into root + two sides, to create tree (heap)
+   var mid = Math.floor((len-2)/2);
+   while(mid >= 0){
+    siftDown(arr, mid--, len-1);    
+  }
+}
+
+
+function siftDown(arr, start, end){
+   var root = start,
+       child = root*2 + 1,
+       toSwap = root;
+   while(child <= end){
+      if(arr[toSwap] < arr[child]){
+        swap(arr, toSwap, child);
+      }
+      if(child+1 <= end && arr[toSwap] < arr[child+1]){
+        swap(arr, toSwap, child+1)
+      }
+      if(toSwap != root){
+         swap(arr, root, toSwap);
+         root = toSwap;
+      }
+      else{
+         return; 
+      }
+      toSwap = root;
+      child = root*2+1
+  }
+}
+        
+
+
