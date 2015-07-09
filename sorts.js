d870b8e1c585f18887c31f43c2c617daf19e0a5c
trebek1 <trebek08@gmail.com>
2015-07-09 15:57:00 -0700

    new quicksort alg

--- sorts.js
+++ sorts.js
@@ -92,6 +92,26 @@ function merge(left, right){
         
 //QuickSort
 
+function quicksort(arr)
+{
+//if array is empty
+if (arr.length === 0) {
+return [];
+}
+  var left = [];
+  var right = [];
+  var pivot = arr[0];
+  //go through each element in array
+  for (var i = 1; i < arr.length; i++) {
+      if (arr[i] < pivot) {
+         left.push(arr[i]);
+      } else {
+         right.push(arr[i]);
+      }
+  }
+  return quicksort(left).concat(pivot, quicksort(right));
+}
+
 function quickSort(arr, left, right){
    var len = arr.length, 
    pivot,
